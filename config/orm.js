9f3254715c23188aa278f0546aca5502ba9de244
Casey Moldavon <cmoldavon@gmail.com>
2020-01-19 20:42:44 -0800

    Adding CRUD related functions to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,38 @@
 const connection = require("../config/connection");
 
 // =========================                ==================================================
+// ========================= Helper Methods ==================================================
+// =========================                ==================================================
+
+//got these functions from activity 17-CatsApp within 13-MVC
+function createQmarks(number){
+    var array = [];
+    for (var i = 0; i < number; i++){
+        array.push("?");
+    }
+    return array.toString();
+}
+
+function objectToSql(object){
+    var array = [];
+    for (var key in object){
+        var value = object[key];
+        if (Object.hasOwnProprty.call(object, key)) {
+            if(typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'" ;
+            }
+            array.push(key + "=" + value)
+        }
+    }
+    return array.toString();
+}
+
+// =========================                ==================================================
 // ========================= CRUD Functions ==================================================
 // =========================                ==================================================
 var orm = {
-    viewTasks: function (table, cb){
-        var dbQuery = "SELECT * FROM " + table + ";";
 
-        console.log(dbQuery);
-        connection.query(dbQuery, function(err, res){
-            if (err) throw err;
-            cb(res);
-        });
-    },
-
+    //Brain method of CRUD functions
     createTask: function(table, cols, vals, cb){
         var dbQuery = "INSERT INTO " + table + "(" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
 
@@ -24,8 +43,18 @@ var orm = {
         });
     },
 
+    viewAllTasks: function (table, cb){
+        var dbQuery = "SELECT * FROM " + table + ";";
+
+        console.log(dbQuery);
+        connection.query(dbQuery, function(err, res){
+            if (err) throw err;
+            cb(res);
+        });
+    },
+
     updateTask: function(table, objColVals, condition, cd){
-        var dbQuery = "UPDATE " + table + " SET " + translateSql(objColVals) + " WHERE " + condition;
+        var dbQuery = "UPDATE " + table + " SET " + objectToSql(objColVals) + " WHERE " + condition;
 
         console.log(dbQuery);
         connection.query(dbQuery, function(err, res){
@@ -44,3 +73,5 @@ var orm = {
         });
     }
 };
+
+module.expores
