e03f1b7dce240460d637fd7f5066d6f4d6d8dcc2
Casey Moldavon <cmoldavon@gmail.com>
2020-01-22 20:12:22 -0800

    Adding to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,36 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "bucket_listDB"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "bucket_listDB"
+// });
 
-connection.connect(function(err){
-    if (err) {
-        console.error(" Error connecting: " + err.stack);
-        return;
-    }
-    else {
-        connection = mysql.createConnection({
-            host: "qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-            user: "esjyuugvnpdnxw83",
-            password: "	oexnd1i79xdr1j2c",
-            port: 3306,
-            database: "kusl7gy4a0fz2yyj"
-        });
-    }
-    console.log("Connection as id: " + connection.threadId);
-});
+// connection.connect(function(err){
+//     if (err) {
+//         console.error(" Error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("Connection as id: " + connection.threadId);
+// });
 
+// module.exports = connection;
+
+
+var connection;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.envJAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "bucket_listDB"
+    });
+};
+
+connection.connect();
 module.exports = connection;
